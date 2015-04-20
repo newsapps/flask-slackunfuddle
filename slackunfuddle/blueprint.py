27370a29efb910570bd0376869c66be1608bcf80
Geoff Hing <geoffhing@gmail.com>
2015-04-20 17:00:09 -0500

    Properly return a response from the view

--- slackunfuddle/blueprint.py
+++ slackunfuddle/blueprint.py
@@ -1,7 +1,7 @@
 """Flask Blueprint to proxy webhooks to Slack"""
 import json
 
-from flask import Blueprint, make_response, request
+from flask import Blueprint, request
 
 from .utils import slack_webhook_url, slack_webhook_post
 from .transform import unfuddle_repo
@@ -17,4 +17,4 @@ def webhook_proxy(id1, id2, id3):
     data = unfuddle_repo(request.data)
     payload = json.dumps(data)
     r = slack_webhook_post(webhook_url, payload)
-    make_response(r.text, r.status_code)
+    return r.text, r.status_code
